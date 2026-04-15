f3f45763ca06b5e6c4e9c9ae9e90bf0a896ca4bb
Parthib <parthibsaha.11sc2020@gmail.com>
2026-04-16 01:51:21 +0530

    Max speed: kick animation speed to 50x

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ function LottieCard({ src, style = {}, cardStyle = {} }) {
       <DotLottieReact
         src={src}
         loop
-        speed={30}
+        speed={50}
         autoplay={false}
         dotLottieRefCallback={setDotLottie}
         style={{ width: "100%", height: "100%", ...style }}
