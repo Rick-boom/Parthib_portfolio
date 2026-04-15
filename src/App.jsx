1cf78b7079c5985518949a788f60f74d148a4843
Parthib <parthibsaha.11sc2020@gmail.com>
2026-04-16 02:10:47 +0530

    Dial animation speed to 10x

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ function LottieCard({ src, style = {}, cardStyle = {} }) {
       <DotLottieReact
         src={src}
         loop
-        speed={15}
+        speed={10}
         autoplay={false}
         dotLottieRefCallback={setDotLottie}
         style={{ width: "100%", height: "100%", ...style }}
